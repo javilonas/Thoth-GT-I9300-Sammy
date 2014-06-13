23e48fb2bce08d3a0bff1f09521846721eae3e3c
javilonas <admin@lonasdigital.com>
2014-06-13 23:04:10 +0200

    Update Ramdisk

--- ramdisk/res/ext/tweaks.sh
+++ ramdisk/res/ext/tweaks.sh
@@ -12,11 +12,8 @@ echo "0" > /proc/sys/vm/drop_caches
 # disable cpuidle log
 echo "0" > /sys/module/cpuidle_exynos4/parameters/log_en
 
-# Mali uV Default (uV off)
-echo "875000 900000 950000 1025000 1075000" > /sys/class/misc/gpu_control/gpu_voltage_control
-
 # Mali GPU_threshold Ahorro Batería
-echo "50% 42% 70% 65% 70% 65% 70% 65%" > /sys/class/misc/gpu_control/gpu_clock_control
+echo "70% 62% 90% 85% 90% 85% 90% 85%" > /sys/class/misc/gpu_control/gpu_clock_control
 
 # Mali GPU_Control 640 MHz (ideal para Gamers)
 echo "266 350 440 533 640" > /sys/class/misc/gpu_control/gpu_clock_control
