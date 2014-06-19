a4696dce6629169017b4717013a78135b6b709c4
javilonas <admin@lonasdigital.com>
2014-06-19 23:41:06 +0200

    Fix LULZACTIVEQ (por si acaso mas adelante se añade por defecto)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
+extern struct cpufreq_governor cpufreq_gov_lulzactiveq;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactiveq)
 #endif
 
 
