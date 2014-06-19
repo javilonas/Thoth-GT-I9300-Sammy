91facfdcd1bfd3705450dd27aeb07ee6623123c8
Christoph Lameter <cl@linux.com>
2014-06-19 19:08:53 +0200

    slub: When allocating a new slab also prep the first object
    We need to branch to the debug code for the first object if we allocate
    a new slab otherwise the first object will be marked wrongly as inactive.
    
    Tested-by: Rabin Vincent <rabin@rab.in>
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -1858,6 +1858,9 @@ new_slab:
 		__SetPageSlubFrozen(page);
 		c->node = page_to_nid(page);
 		c->page = page;
+
+		if (kmem_cache_debug(s))
+			goto debug;
 		goto load_freelist;
 	}
 	if (!(gfpflags & __GFP_NOWARN) && printk_ratelimit())
