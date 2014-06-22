8760b71fa0f22d7326635c58b99e3d1a4c3e0f13
javilonas <admin@lonasdigital.com>
2014-06-23 00:45:16 +0200

    Fix build

--- arch/arm/mvp/mvpkm/mvpkm_main.c
+++ arch/arm/mvp/mvpkm/mvpkm_main.c
@@ -2162,9 +2162,7 @@ SetupMonitor(struct MvpkmVM *vm)
 		register uint32 r4 asm("r4") = hsctlr;
 
 		asm volatile (
-#if USE_ARCH_EXTENSION_SEC
-			".arch_extension sec\n\t"
-#endif
+			".arch_extension sec\n"
 			"smc 0"
 			:
 			: "r" (r0), "r" (r1), "r" (r2), "r" (r3), "r" (r4)
