6611eaa9f863f4ac2358395ff541f12ee6827d12
javilonas <admin@lonasdigital.com>
2014-06-28 20:44:05 +0200

    FIX 'Algunos arreglos en lowmemorykiller (test)'

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -100,7 +100,7 @@ static int lowmem_adj[6] = {
 	16,
 	17,
 };
-static int lowmem_adj_size = 4;
+static int lowmem_adj_size = 6;
 static size_t lowmem_minfree[6] = {
 	3 * 512,	/* 6MB */
 	2 * 1024,	/* 8MB */
