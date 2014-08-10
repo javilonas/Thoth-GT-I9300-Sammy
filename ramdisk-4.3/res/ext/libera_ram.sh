352ac27f2f391f0913ccf3617d7994239c4c07d2
javilonas <admin@lonasdigital.com>
2014-08-10 15:48:40 +0200

    Fix liberar swap

--- ramdisk-4.3/res/ext/libera_ram.sh
+++ ramdisk-4.3/res/ext/libera_ram.sh
@@ -1,6 +1,6 @@
 #!/sbin/busybox sh
 #
-# libera pagecache cada 3 horas si esta está por debajo de 20360 kbytes
+# libera pagecache cada 3 horas si esta está por debajo de 102400 kbytes (100 MB)
 # 
 
 /sbin/busybox renice 19 `pidof libera_ram.sh`
@@ -11,13 +11,25 @@ FREE=`free -m | grep -i mem | awk '{print $4}'`
 
 while [ 1 ];
 do
-        if [ $FREE -lt 20360 ]; then
-                sync
-                sleep 3
+        if [ $FREE -lt 102400 ]; then
+                /sbin/busybox sync
+                /sbin/busybox sleep 3
 		echo $DROP_ONE > /proc/sys/vm/drop_caches
 		echo $DROP_TWO > /proc/sys/vm/drop_caches
 		echo $DROP_THREE > /proc/sys/vm/drop_caches
-		sync; echo $DROP_THREE > /proc/sys/vm/drop_caches
+		/sbin/busybox sync; echo $DROP_THREE > /proc/sys/vm/drop_caches
+                /sbin/busybox sync
+		/sbin/busybox swapoff /dev/block/vnswap0 > /dev/null 2>&1
+		/sbin/busybox swapoff /dev/block/zram0 > /dev/null 2>&1
+		/sbin/busybox swapoff /dev/block/zram1 > /dev/null 2>&1
+		/sbin/busybox swapoff /dev/block/zram2 > /dev/null 2>&1
+		/sbin/busybox swapoff /dev/block/zram3 > /dev/null 2>&1
+		/sbin/busybox swapon -p 2 /dev/block/vnswap0 > /dev/null 2>&1
+		/sbin/busybox swapon -p 2 /dev/block/zram0 > /dev/null 2>&1
+		/sbin/busybox swapon -p 2 /dev/block/zram1 > /dev/null 2>&1
+		/sbin/busybox swapon -p 2 /dev/block/zram2 > /dev/null 2>&1
+		/sbin/busybox swapon -p 2 /dev/block/zram3 > /dev/null 2>&1
+                /sbin/busybox sync
         fi
 sleep 10800
 done
