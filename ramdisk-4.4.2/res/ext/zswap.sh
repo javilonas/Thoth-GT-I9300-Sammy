b2711fcbf24f6284dd6caa006a721816d47e4718
javilonas <admin@lonasdigital.com>
2014-08-30 09:30:58 +0200

    Fix Zswap

--- ramdisk-4.4.2/res/ext/zswap.sh
+++ ramdisk-4.4.2/res/ext/zswap.sh
@@ -8,7 +8,7 @@ ZSWAPSIZE=$(($RAMSIZE*100))
 
 if [ $ZSWAPSIZE -gt 0 ]; then
 echo `expr $ZSWAPSIZE \* 1024 \* 1024` > /sys/devices/virtual/block/vnswap0/disksize
-/sbin/busybox swapoff /dev/block/zram0 > /dev/null 2>&1
+/sbin/busybox swapoff /dev/block/vnswap0 > /dev/null 2>&1
 /sbin/busybox mkswap /dev/block/vnswap0 > /dev/null 2>&1
 /sbin/busybox swapon /dev/block/vnswap0 > /dev/null 2>&1
 fi
